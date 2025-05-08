13b855e82e258616c6ee69066a9a2ce62b3371eb
H-Munene <hezemunene01@gmail.com>
2025-05-08 10:07:27 +0300

    feat: colors, dimensions, theme classes

---
+++ lib/core/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.teal;
+  static Color errorColor = Colors.redAccent;
+  static Color borderColor = Colors.grey;
+  static Color selectedItemColor = Colors.black;
+  static Color unselectedItemColor = Colors.grey;
+  static Color labelColor = const Color.fromARGB(255, 160, 159, 159);
+}
---
+++ lib/core/dimensions.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AppDimensions {
+  static BorderRadius circleBorderRadius = BorderRadius.circular(12);
+  static BorderRadius unfocussedCircleBorderRadius = BorderRadius.circular(7);
+  static BorderRadius focussedCircleBorderRadius = BorderRadius.circular(12);
+
+  static const EdgeInsets textfieldVerticalSpacing = EdgeInsets.symmetric(
+    vertical: 10,
+  );
+
+  static const double iconSize = 22;
+}
